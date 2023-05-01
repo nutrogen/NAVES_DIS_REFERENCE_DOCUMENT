8bea2ed9db71eaf0caf081b341e0f579466ddfe0
nutrogen <81790274+nutrogen@users.noreply.github.com>
2023-05-01 12:53:31 +0900

    Add files via upload

---
+++ DisReferenceDocumentController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using NavesPortalforWebWithCoreMvc.Controllers.AuthFromIntranetController;
+
+namespace NavesPortalforWebWithCoreMvc.Controllers.DIS
+{
+    [Authorize]
+    [CheckSession]
+    public class DisReferenceDocumentController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return await Task.Run(() => View());
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            return await Task.Run(() => View());
+        }
+
+        public async Task<IActionResult> Detail()
+        {
+            return await Task.Run(() => View());
+        }
+    }
+}
